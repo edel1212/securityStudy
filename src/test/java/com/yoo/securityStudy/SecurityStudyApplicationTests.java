1abb29342e1fc719419bee5726586da9d0461a33
yoo <edel1212@naver.com>
2024-03-19 23:23:17 +0900

    project init

---
+++ src/test/java/com/yoo/securityStudy/SecurityStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoo.securityStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
