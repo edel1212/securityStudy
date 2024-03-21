2a86e486897c69da2b71d667b1df478b65045d13
yoo <edel1212@naver.com>
2024-03-21 22:37:09 +0900

    Access Test Conroller

---
+++ src/main/java/com/yoo/securityStudy/AccessController.java
@@ -0,0 +1,29 @@
+package com.yoo.securityStudy;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccessController {
+
+    @GetMapping("/all")
+    public ResponseEntity allAccess(){
+        return ResponseEntity.ok("All Access!!");
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity userAccess(){
+        return ResponseEntity.ok("User Access!!");
+    }
+
+    @GetMapping("/manager")
+    public ResponseEntity managerAccess(){
+        return ResponseEntity.ok("manager Access!!");
+    }
+
+    @GetMapping("/admin")
+    public ResponseEntity adminAccess(){
+        return ResponseEntity.ok("admin Access!!");
+    }
+}
