3bc2883ac6552680d5446ccea434730870776470
yoo <edel1212@naver.com>
2024-03-26 22:33:47 +0900

    add user 상속 사용 이유  주석

--- src/main/java/com/yoo/securityStudy/dto/MemberDTO.java
+++ src/main/java/com/yoo/securityStudy/dto/MemberDTO.java
@@ -8,6 +8,14 @@ import org.springframework.security.core.userdetails.User;
 import java.util.Collection;
 import java.util.Set;
 
+/**
+ * extends User 를 사용하는 이유는 간단하다
+ * UserDetails를 반환하는 loadUserByUsername()메서드에서
+ * - 아이디, 비밀번호, 권한 << 이렇게 3개만 있으면 User를 사용해도 되지만
+ *
+ * 그렇지 않을 경우 추가적은 정보를 갖는 경우 아래와 같이 DTO를 추가후 Super()를 통해
+ * 부모에게 필요한 생성정보를 전달 하고 나머지는 내가 필요한 정보를 들고 있기 위함이다.
+ * */
 @Getter
 @Setter
 @ToString
