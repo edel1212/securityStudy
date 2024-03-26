d6a90c3a08bc78fdd852f8e8db8a55231a457de3
yoo <edel1212@naver.com>
2024-03-26 22:31:16 +0900

    add extends User DTO

--- src/main/java/com/yoo/securityStudy/dto/MemberDTO.java
+++ src/main/java/com/yoo/securityStudy/dto/MemberDTO.java
@@ -1,20 +1,33 @@
 package com.yoo.securityStudy.dto;
 
 import com.yoo.securityStudy.entity.enums.Roles;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 
+import java.util.Collection;
 import java.util.Set;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-@Data
-public class MemberDTO {
+@Getter
+@Setter
+@ToString
+public class MemberDTO extends User {
     private String id;
     private String password;
     private String name;
     private Set<Roles> roles;
+
+    @Builder
+    public MemberDTO(String id
+            , String password
+            , Collection<? extends GrantedAuthority> authorities
+            , Set<Roles> roles
+            , String name) {
+        super(id, password, authorities);
+        this.id = id;
+        this.password = password;
+        this.name = name;
+        this.roles = roles;
+    }
+
 }
