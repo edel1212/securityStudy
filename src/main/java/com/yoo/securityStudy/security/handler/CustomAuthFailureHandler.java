078ee55409dc5d3dbbaaf4f6c84b3f00e12c885e
yoo <edel1212@naver.com>
2024-04-17 22:43:04 +0900

    custom auth failure handler 추가

--- src/main/java/com/yoo/securityStudy/security/handler/CustomAuthFailureHandler.java
+++ src/main/java/com/yoo/securityStudy/security/handler/CustomAuthFailureHandler.java
@@ -1,10 +1,14 @@
 package com.yoo.securityStudy.security.handler;
 
-import jakarta.servlet.ServletException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yoo.securityStudy.exception.dto.ErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
@@ -14,7 +18,23 @@ import java.io.IOException;
 @Component
 public class CustomAuthFailureHandler extends SimpleUrlAuthenticationFailureHandler {
     @Override
-    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-        super.onAuthenticationFailure(request, response, exception);
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
+        log.info("- Custom Auth Failure Handler 접근 -");
+        var objectMapper = new ObjectMapper();
+        String errorMessage;
+        if (exception instanceof BadCredentialsException) {
+            errorMessage = "아이디와 비밀번호를 확인해주세요.";
+        } else if (exception instanceof InternalAuthenticationServiceException) {
+            errorMessage = "내부 시스템 문제로 로그인할 수 없습니다. 관리자에게 문의하세요.";
+        } else if (exception instanceof UsernameNotFoundException) {
+            errorMessage = "존재하지 않는 계정입니다.";
+        } else {
+            errorMessage = "알 수없는 오류입니다.";
+        }
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .code(HttpServletResponse.SC_UNAUTHORIZED)
+                .message(errorMessage)
+                .build();
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
     }
 }
