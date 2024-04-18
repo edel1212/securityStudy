44cfc45fa3f5d0a2c174c608c04ef7d0397657fd
yoo <edel1212@naver.com>
2024-04-18 22:44:01 +0900

    jwt token DTO cnrk

---
+++ src/main/java/com/yoo/securityStudy/security/dto/jwtToken.java
@@ -0,0 +1,19 @@
+package com.yoo.securityStudy.security.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class jwtToken {
+    // Jwt 인증 타입 [ Bearer 사용 ]
+    private String grantType;
+    // 발급 토근
+    private String accessToken;
+    // 리프레쉬 토큰
+    private String refreshToken;
+}
