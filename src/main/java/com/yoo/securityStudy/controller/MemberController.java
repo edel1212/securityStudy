d6cc67d4c50a58fc846438cfb2fe12ea3abbdf51
jeoungho.yoo <jeoungho.yoo@stans.co.kr>
2024-04-24 17:46:02 +0900

    미사용 DTO 제거

--- src/main/java/com/yoo/securityStudy/controller/MemberController.java
+++ src/main/java/com/yoo/securityStudy/controller/MemberController.java
@@ -1,28 +1,18 @@
 package com.yoo.securityStudy.controller;
 
 import com.yoo.securityStudy.config.JwtUtil;
-import com.yoo.securityStudy.dto.JwtLoginDTO;
 import com.yoo.securityStudy.dto.LoginDTO;
-import com.yoo.securityStudy.dto.MemberDTO;
-import com.yoo.securityStudy.entity.enums.Roles;
 import com.yoo.securityStudy.security.dto.JwtToken;
-import com.yoo.securityStudy.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @RequestMapping(value = "/member", produces = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
