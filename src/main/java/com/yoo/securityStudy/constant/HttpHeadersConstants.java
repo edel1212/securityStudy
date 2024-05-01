be00e75f9042f7df379d1a35144b3f67db8882d8
yoo <edel1212@naver.com>
2024-05-01 14:10:37 +0900

    Http Header 상수 추가

---
+++ src/main/java/com/yoo/securityStudy/constant/HttpHeadersConstants.java
@@ -0,0 +1,7 @@
+package com.yoo.securityStudy.constant;
+
+public class HttpHeadersConstants {
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String CONTENT_TYPE = "Content-Type";
+    public static final String ACCEPT = "Accept";
+}
