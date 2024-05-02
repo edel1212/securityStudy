3bd36057edb4875e534e95e70fa58997d9151c2a
yoo <edel1212@naver.com>
2024-05-02 23:15:53 +0900

    doFilterInternal 메서드 구현

--- src/main/java/com/yoo/securityStudy/security/filter/jwtFilter.java
+++ src/main/java/com/yoo/securityStudy/security/filter/jwtFilter.java
@@ -6,6 +6,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -22,9 +24,16 @@ public class jwtFilter extends OncePerRequestFilter {
         // 1. Request Header 에서 JWT 토큰 추출
         String token = jwtUtil.resolveToken(request);
         
-        // 2. Token 유효값 검사
+        // 2. Token 유효값 검사 - 이상이 있을 경우 Exception Handler 적용
         jwtUtil.validateToken(token);
 
-
+        try {
+                // 토큰이 유효할 경우 토큰에서 Authentication 객체를 가지고 와서 SecurityContext 에 저장
+                Authentication authentication = jwtUtil.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        filterChain.doFilter(request, response);
     }
 }
