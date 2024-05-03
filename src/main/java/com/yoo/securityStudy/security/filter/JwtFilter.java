b172a1693df159c62bcccf9f755bf7f689462047
jeoungho.yoo <jeoungho.yoo@stans.co.kr>
2024-05-03 16:53:11 +0900

    에러 수정

--- src/main/java/com/yoo/securityStudy/security/filter/JwtFilter.java
+++ src/main/java/com/yoo/securityStudy/security/filter/JwtFilter.java
@@ -1,158 +1,41 @@
-package com.yoo.securityStudy.config;
+package com.yoo.securityStudy.security.filter;
 
-import com.yoo.securityStudy.security.dto.JwtToken;
-import io.jsonwebtoken.*;
+import com.yoo.securityStudy.config.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.time.Instant;
-import java.time.ZonedDateTime;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Date;
-import java.util.stream.Collectors;
+import java.io.IOException;
 
-import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-
-@Log4j2
 @Component
-public class JwtUtil {
-    @Value("${jwt.expiration_time}")
-    private Long accessTokenExpTime;
-    @Value("${jwt.secret}")
-    private String secret;
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
 
-    /**
-     * createAccessToken 이슈로 인해 재생성 중
-     *
-     * - 👉 Authentication을 통해 로그인한 정보를 받아서 사용이 가능하다!!
-     * */
-    public JwtToken generateToken(Authentication authentication){
+    private final JwtUtil jwtUtil;
 
-        // 로그인에 성공한 사용자의 권한을 가져온 후 문자열로 반환
-        // ex) "ROLE_USER,ROLE_MANAGER,ROLE_ADMIN"
-        String authorities = authentication.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.joining(","));
-        // 로그인에 성공한 계정Id
-        String userName = authentication.getName();
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // 1. Request Header 에서 JWT 토큰 추출
+        String token = jwtUtil.resolveToken(request);
+        // 2. token이 없을 경우 해당 필터 스킵
+        if(token == null) filterChain.doFilter(request, response);
+        // 3. Token 유효값 검사 - 이상이 있을 경우 Exception Handler 적용
+        jwtUtil.validateToken(token);
 
-        // 토큰 만료시간 생성
-        ZonedDateTime now = ZonedDateTime.now();
-        ZonedDateTime tokenValidity = now.plusSeconds(this.accessTokenExpTime);
-
-        Claims claims = Jwts.claims();
-        claims.put("memberId", userName);
-        claims.put("auth", authorities);
-
-        // Jwt AccessToken 생성
-        String accessToken =  Jwts.builder()
-                .setClaims(claims)
-                .setIssuedAt(Date.from(Instant.now()))
-                .setExpiration(Date.from(tokenValidity.toInstant()))
-                .signWith(SignatureAlgorithm.HS256, secret)
-                .compact();
-
-        // Refresh Token 생성
-        // 토큰 만료시간 생성
-        ZonedDateTime reNow = ZonedDateTime.now();
-        ZonedDateTime reTokenValidity = reNow.plusSeconds(this.accessTokenExpTime);
-        String refreshToken = Jwts.builder()
-                .setExpiration(Date.from(reTokenValidity.toInstant()))
-                .signWith(SignatureAlgorithm.HS256, secret)
-                .compact();
-
-        return JwtToken.builder()
-                .grantType("Bearer")
-                .accessToken(accessToken)
-                .refreshToken(refreshToken)
-                .build();
-    }
-
-    /**
-     * JWT 검증
-     * - 각각 예외에 따라 ControllerAdvice를 사용해서 처리가 가능함
-     * @param accessToken
-     * @return IsValidate
-     */
-    public boolean validateToken(String accessToken) {
         try {
-            Jwts.parserBuilder().setSigningKey(secret).build().parseClaimsJws(accessToken);
-            return true;
-        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
-            log.info("Invalid JWT Token", e);
-        } catch (ExpiredJwtException e) {
-            log.info("Expired JWT Token", e);
-        } catch (UnsupportedJwtException e) {
-            log.info("Unsupported JWT Token", e);
-        } catch (IllegalArgumentException e) {
-            log.info("JWT claims string is empty.", e);
-        } // try - catch
-        return false;
+            // 토큰이 유효할 경우 토큰에서 Authentication 객체를 가지고 와서 SecurityContext 에 저장
+            Authentication authentication = jwtUtil.getAuthentication(token);
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        filterChain.doFilter(request, response);
     }
-
-    /**
-     * JWT Claims 추출
-     * @param accessToken
-     * @return JWT Claims
-     */
-    private Claims parseClaims(String accessToken) {
-        try {
-            return Jwts.parserBuilder()
-                    .setSigningKey(secret)
-                    .build()
-                    .parseClaimsJws(accessToken)
-                    .getBody();
-        } catch (ExpiredJwtException e) {
-            return e.getClaims();
-        }// try - catch
-    }
-
-    /**
-     * JWT 값 추출
-     * @param request
-     * @return String Jwt Token 원문 값
-     */
-    public String resolveToken(HttpServletRequest request) {
-        String bearerToken = request.getHeader(AUTHORIZATION);
-        if (bearerToken == null || !bearerToken.startsWith("Bearer ")) return null;
-        return bearerToken.replaceAll("Bearer ","");
-    }
-
-
-    public Authentication getAuthentication(String accessToken) {
-        // 1 . 토큰에서 Claims 값을 가져온다. - 내가 넣은 값이 들어있음
-        Claims claims = this.parseClaims(accessToken);
-
-        // 2 . 주입된 토큰에서 내가 넣은 값의 유무를 체크
-        if(claims.get("memberId") == null || claims.get("auth") == null) {
-            // 예외 발생 시켜 처리하자
-            throw new RuntimeException();
-        }// if
-
-        // 3 . claims에서 권한 정보 추출 후 Spring Security의 권한 형식에 맞게 변환
-        //   ⭐️ jwt에 등록된 권한은 Security자체에서 주입된 값이기에 ROLE_가 prefix로 붙어있다!
-        //      ex) ROLE_ADMIN, ROLE_USER
-        Collection<? extends GrantedAuthority> authorities =
-                Arrays.stream(claims.get("auth").toString().split(","))
-                        .map(SimpleGrantedAuthority::new)
-                        .collect(Collectors.toList());
-        // 계정ID
-        String username = claims.get("memberId").toString();
-
-        // 4 . UserDetail 객체 생성
-        UserDetails principal = new User(username, "", authorities);
-
-        // 반환
-        return new UsernamePasswordAuthenticationToken(principal, "", authorities);
-    }
-
 }
