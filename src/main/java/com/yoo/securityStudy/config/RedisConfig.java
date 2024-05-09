274313c37c3eb99fcec80d9c8cbbc9e263ceee65
yoo <edel1212@naver.com>
2024-05-09 22:01:55 +0900

    코드 설명 주석 추가

--- src/main/java/com/yoo/securityStudy/config/RedisConfig.java
+++ src/main/java/com/yoo/securityStudy/config/RedisConfig.java
@@ -24,17 +24,27 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
+        // 독립형 Redis 인스턴스에 대한 연결 설정을 위한 인스턴스 생성
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        // 호스트 주소 설정
         redisStandaloneConfiguration.setHostName(redisHost);
+        // 포트번호 설정
         redisStandaloneConfiguration.setPort(redisPort);
+        // 패스워드 설정
         redisStandaloneConfiguration.setPassword(redisPassword);
-        return  new LettuceConnectionFactory(redisStandaloneConfiguration);
+        // Lettuce Redis 클라이언트를 사용하여 Redis에 연결하는 데 사용됩니다.
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
+
     @Bean
     public RedisTemplate<String, String> redisTemplate() {
+        // 사용할 RedisTemplate 객체 생성
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        // RedisTemplate이 사용할 Connection Factory를 설정합니다. 앞서 정의한 Redis 연결 팩토리를 생성하는 메서드를 적용
+        redisTemplate.setConnectionFactory(this.redisConnectionFactory());
+        // Key Serializer를 설정합니다. 문자열을 직렬화합니다.
         redisTemplate.setKeySerializer(new StringRedisSerializer());
+        // Value Serializer를 설정합니다. 문자열을 직렬화합니다
         redisTemplate.setValueSerializer(new StringRedisSerializer());
         return redisTemplate;
     }
